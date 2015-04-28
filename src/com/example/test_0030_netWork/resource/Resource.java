ad64d35dc017cbc3fe9bb3c012f0e02faa7f05cf
baohua00_work <mo369258147@163.com>
2015-04-28 22:18:32 +0800

    科大国创ICT签到APP

--- src/com/example/test_0030_netWork/resource/Resource.java
+++
@@ -1,6 +0,0 @@
-package com.example.test_0030_netWork.resource;
-
-public class Resource {
-	public static String[] TITLES = {"Title_1","Titl_2","Titl_3","Titl_4","Titl_5","Titl_6"};
-	public static String[] DIALOGUE = {"Text_1","Text_2","Text_3","Text_4","Text_5","Text_6"};
-}
